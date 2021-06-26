328de24748dad5117fb8783e065b1b2b5e0883a5
fox-io <gitlab.fox-io@foxdata.io>
2021-06-26 08:44:25 -0500

    Day 7 - Cleaned up comments more.

--- day_007.py
+++ day_007.py
@@ -1,20 +1,30 @@
 """
-Day 5 Project: Hangman Game
+Day 7 Project: Hangman Game
 
 (c)2021 John Mann <gitlab.fox-io@foxdata.io>
 """
 import random
 
-# TODO: Expand our word list.
+"""
+List of words to randomly choose from when creating a game.
+
+TODO: Expand our word list.
+"""
 word_list = ["aardvark", "baboon", "camel"]
 
+"""
+Dict containing the alphabet and flags indicating if they are still available for guesses.
+The letters are set to False after the user guesses them.
+"""
 available_letters = {"a": True, "b": True, "c": True, "d": True, "e": True, "f": True, "g": True, "h": True, "i": True,
                      "j": True, "k": True, "l": True, "m": True, "n": True, "o": True, "p": True, "q": True, "r": True,
                      "s": True, "t": True, "u": True, "v": True, "w": True, "x": True, "y": True, "z": True}
 
+"""
+Dict containing body parts and flags to denote if they are visible or not.
+We use this to draw the gallows in the function update_gallows
+"""
 the_hangman = {
-    # Dict containing body parts and flags to denote if they are visible or not.
-    # We use this to draw the gallows in the function update_gallows
     "head": [False, "o"],
     "arm1": [False, "/"],
     "body": [False, "|"],
@@ -25,7 +35,15 @@ the_hangman = {
 
 
 def update_gallows(man):
-    # Assembles the gallows, adding in body parts of the hangman if they are flagged as visible.
+    """
+    Assembles the gallows, adding in body parts of the hangman if they are flagged as visible.
+
+    :param man: Dict containing the hangman's body parts and current state flags.
+
+    :return new_gallows: Formatted string which displays the gallows & the hangman's current state.
+
+    :rtype string:
+    """
     new_gallows = " ┌────┐\n"
     new_gallows = new_gallows + " │    "
     if man['head'][0]:
@@ -61,9 +79,11 @@ def update_gallows(man):
 
 def mask_random_word():
     """
-    mask_random_word()
-
     Formats and returns the random word with underscores instead of letters for letters which have not yet been guessed.
+
+    :return masked_word:
+
+    :rtype string:
     """
     masked_word = ""
     # Loop through each letter of random_word
@@ -79,6 +99,9 @@ def mask_random_word():
 
 
 def show_available_letters():
+    """
+    Prints the alphabet, crossing out any letters which have been guessed already.
+    """
     letter_counter = 0
     # Generates a list of the alphabet, striking through any letters that have been used.
     available_letter_string = ""
@@ -95,6 +118,12 @@ def show_available_letters():
 
 
 def is_game_over():
+    """
+    Checks the hangman's state to determine if he is completely visible (True), or if we still have
+    more body parts to add (False)
+    :return True|False: Returns True if the hangman is hung, False if he is not.
+    :rtype bool:
+    """
     # Check each body part to determine if we have any still left hidden.
     for body_part in the_hangman:
         # If even a single body part is set to False, the game is not over.
@@ -105,20 +134,32 @@ def is_game_over():
 
 
 def is_word_guessed():
+    """
+    Checks our word to see if the user has revealed all of the letters in it. If not, the game continues.
+    :return True|False: Returns True if the word is revealed, False if there are still letters hidden.
+    :rtype bool:
+    """
     for position in range(0, len(random_word)):
         if available_letters[random_word[position]]:
             return False
     return True
 
 
-# Main Game Loop
-# Game Setup
-# ----------
+"""
+Main Game Loop
+"""
+
+"""
+Game Setup
+----------
+"""
 # Choose a random word from word_list
 random_word = word_list[random.randint(0, len(word_list) - 1)]
 while True:
-    # Display Game Board
-    # ------------------
+    """
+    Display Game Board
+    ------------------
+    """
     # Show game board
     print(update_gallows(the_hangman))
     # Display the available letters
@@ -133,8 +174,10 @@ while True:
         print("The hangman is hung!")
         break
 
-    # Process User Input
-    # ------------------
+    """
+    Process User Input
+    ------------------
+    """
     while True:
         # Ask user for a letter and check if it is in the random word
         user_choice = input("Guess a letter that you think might be in the word.\n: ").lower()
