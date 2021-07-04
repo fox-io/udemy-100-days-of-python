0a24228b32bd66ab07e19a2c3046802881bd96e1
fox-io <gitlab.fox-io@foxdata.io>
2021-07-04 07:26:41 -0500

    Day 15: - Coffee Machine pseudocode.

--- day_015.py
+++ day_015.py
@@ -1,17 +1,47 @@
 """
 -----
-Day 15 Project: Coffee Maker
+Day 15 Project: Coffee Machine
 -----
+Coffee Machine Program Requirements
+
+1.Prompt user by asking "What would you like? (espresso/latte/cappuccino): "
+    a.Check the user’s input to decide what to do next.
+    b.The prompt should show every time action has completed, e.g. once the drink is dispensed.
+      The prompt should show again to serve the next customer.
+2.Turn off the Coffee Machine by entering "off" to the prompt.
+    a.For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine.
+      Your code should end execution when this happens.
+3.Print report.
+    a.When the user enters “report” to the prompt, a report should be generated that shows the current resource values.
+      e.g. Water: 100ml Milk: 50ml Coffee: 76g Money: $2.5
+4.Check resources sufficient?
+    a.When the user chooses a drink, the program should check if there are enough resources to make that drink.
+    b.E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should not continue to make
+      the drink but print: "Sorry there is not enough water."
+    c.The same should happen if another resource is depleted, e.g. milk or coffee.
+5.Process coins.
+    a.If there are sufficient resources to make the drink selected, then the program should prompt the user to insert
+      coins.
+    b.Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+    c.Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2pennies =
+      0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+6.Check transaction successful?
+    a.Check that the user has inserted enough money to purchase the drink they selected.
+      E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the program should say
+      "Sorry that's not enough money. Money refunded.".
+    b.But if the user has inserted enough money, then the cost of the drink gets added to the machine as the profit
+      and this will be reflected the next time “report” is triggered. E.g.Water: 100mlMilk: 50mlCoffee: 76gMoney: $2.5
+    c.If the user has inserted too much money, the machine should offer change. E.g. “Here is $2.45 dollars in change.”
+      The change should be rounded to 2 decimal places.
+7.Make Coffee.
+    a.If the transaction is successful and there are enough resources to make the drink the user selected, then the
+      ingredients to make the drink should be deducted from the coffee machine resources.E.g. report before
+      purchasing latte: Water: 300ml Milk: 200ml Coffee: 100g Money: $0 Report after purchasing latte: Water: 100ml
+      Milk: 50ml Coffee: 76g Money: $2.5
+    b.Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If latte was their choice
+      of drink.
 
 (c)2021 John Mann <gitlab.fox-io@foxdata.io>
-"""
-PRESS_ENTER = "Press enter to continue..."
-MENU = "[1] Play\n[0] Quit\n: "
-INVALID_INPUT = "Please choose a menu option to continue."
-PLAY_GAME = 1
-QUIT_GAME = 0
-LOGO = """
-Coffee Maker
 """
 COFFEE_MENU = {
     "espresso": {
@@ -47,16 +77,24 @@ resources = {
 
 
 def main():
-    print(LOGO)
-    while True:
-        menu_command = int(input(MENU))
-        if menu_command == PLAY_GAME:
-            pass
-        elif menu_command == QUIT_GAME:
-            return
-        else:
-            print(INVALID_INPUT)
-
+    pass
+    root_loop:
+    show_coffee_menu()
+    get_user_menu_input()
+        if "off": turn_coffee_machine_off()
+        if "report": generate_report()
+        if make_coffee:
+            has_enough_resources()
+            if not: error_not_enough_resources()
+            else:
+                until enough_money():
+                    insert_money()
+                        insert quarters() insert dimes() insert nickels() insert pennies()
+                move_money_to_profit()
+                give_change()
+                if transaction_successful and enough_resources:
+                    reduce_resources(drink_type)
+                    dispense_coffee(drink_type)
 
 if __name__ == "__main__":
     main()
