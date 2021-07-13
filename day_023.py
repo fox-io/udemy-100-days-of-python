1ef8f662cee83be5bf15e220128a0edd4f1edf75
fox-io <gitlab.fox-io@foxdata.io>
2021-07-13 07:22:00 -0500

    Day 23: - Added level indicator - Added player

--- day_023.py
+++ day_023.py
@@ -7,23 +7,69 @@ from random import randint
 from time import sleep
 
 
-class TurtleCrossing:
+class Player(Turtle):
     def __init__(self):
+        super().__init__("turtle")
+        self.setheading(90.0)
+        self.penup()
+        self.goto(0, -250)
+
+
+class Level(Turtle):
+    def __init__(self):
+        super().__init__()
+        # Default to level 1
+        self.level = 1
+
+        # Hide turtle, move to correct location
+        self.hideturtle()
+        self.penup()
+        self.goto(-250, 270)
+
+        # Manually update on creation
+        self.update()
+
+    def set(self, new_level):
+        # Set new level and update display
+        self.level = new_level
+        self.update()
+
+    def get(self):
+        return self.level
+
+    def update(self):
+        self.clear()
+        self.write(f"Level: {self.level}", False, align="center", font=("Monospace", 16, "normal"))
+
+
+class TurtleCrossing:
+    """The main game class."""
+    def __init__(self):
+        # Create and set up the game window.
+        self.running = True
         self.screen = Screen()
         self.screen.setup(600, 600)
         self.screen.tracer(0)
+        self.screen.title("Turtle Crossing")
+
+        # Add level indicator
+        self.level = Level()
+
+        # Add player
+        self.player = Player()
 
     def update(self):
+        # Update the screen
         self.screen.update()
-        return False
+        # Exit the game
+        self.running = False
 
 
 def main():
     game = TurtleCrossing()
-    running = True
-    while running:
+    while game.running:
         sleep(0.1)
-        running = game.update()
+        game.update()
     game.screen.exitonclick()
 
 
